58dc4cd6933a58f1c3ca2fbbe20253e9b621b7d4
Lucas Nicolás Ibarra <niicoibarra1@gmail.com>
2021-07-13 19:01:48 -0300

    Vista principal casi lista

--- src/Components/FE_Body.jsx
+++ src/Components/FE_Body.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -13,28 +12,66 @@ import Grid from '@material-ui/core/Grid'
 
 const useStyles = makeStyles({
     root: {
-      maxWidth: 'auto',
+      width: '70%',
+      marginBottom: '3em'
     },
 });
 
 export default function FE_Body(prop) {
-
     const classes = useStyles();
     const [animalsCardsList, setAnimalsCardsList] = useState([]);
-
-    const ApiCall = apiUrl => {
-        axios(apiUrl)
-        .then(response => setAnimalsCardsList(response))
+    
+    const renderAnimalCards = (apiRequestAnswer) => {
+        console.log(apiRequestAnswer);
+        setAnimalsCardsList(
+            apiRequestAnswer.map((current, id) => {
+                return (
+                    <Grid
+                        key={current.id}
+                        container
+                        spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Card className={classes.root}>
+                            <CardMedia
+                                component="img"
+                                alt={current.nombre}
+                                height="300em"
+                                image={current.fotos[2].imagen}
+                                title={current.nombre}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {current.nombre}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {current.descripcion}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Ver más
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                )
+            })
+        )
     }
+    const apiCall = () => {
+        axios({
+            method: 'get',
+            url: 'https://localhost:5001/api/mascotas'
+        })
+        .then(acceptedAnswer => renderAnimalCards(acceptedAnswer.data))
+        .catch(rejectedAnswer => console.log('Error al obtener los datos - ', rejectedAnswer));
+    }
+    useEffect(() => apiCall(), []);
 
-    useEffect(() => {
-        const apiUrl = "https://localhost:5001/api/mascotas";
-        ApiCall(apiUrl);
-        console.log(animalsCardsList)
-    }, [])
-
-    if (prop.idBody == 'StartBody'){
-
+    if (prop.idBody === 'StartBody'){
         return (
             <div>
                 <p style={{
@@ -43,43 +80,8 @@ export default function FE_Body(prop) {
                         fontSize: 'large',
                         marginTop: '3em'
                 }}>A continuación te mostramos los animales que esperan ser parte de tu familia:</p>
-
-                {animalsCardsList}
-                <Grid
-                    container
-                    spacing={0}
-                    direction="column"
-                    alignItems="center"
-                    justify="center"
-                >
-                    <Card className={classes.root}>
-                        <CardActionArea>
-                            <CardMedia
-                            component="img"
-                            alt="Contemplative Reptile"
-                            height="300"
-                            image="/static/images/cards/contemplative-reptile.jpg"
-                            title="Contemplative Reptile"
-                            />
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Lizard
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                                across all continents except Antarctica
-                            </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                            <Button size="small" color="primary">
-                                Ver más
-                            </Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
+                {animalsCardsList}                
             </div>
         )
     }
-
 }
--- src/Components/FE_Header.jsx
+++ src/Components/FE_Header.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 
 export default function FE_Header(prop) {
 
-    if (prop.idHeader == 'StartHeader'){
+    if (prop.idHeader === 'StartHeader'){
         return (
             <div>
-                <div className="">
-                    <h1 style={{
-                        textAlign: 'center', 
-                        textDecoration: 'underline',
-                        fontFamily: 'Segoe UI'
-                    }}>!Bienvenido a Mascotitas!</h1>
-                    <p style={{
-                        fontFamily: 'Segoe UI',
-                        textAlign: 'left',
-                        marginTop: '1em'
+                <h1 style={{
+                    textAlign: 'center', 
+                    textDecoration: 'underline',
+                    fontFamily: 'Segoe UI'
+                }}>!Bienvenido a Mascotitas!</h1>
+                <p style={{
+                    fontFamily: 'Segoe UI',
+                    textAlign: 'left',
+                    marginTop: '1em'
                 }}>Somos una organización sin fines de lucro que busca que animales sin hogar encuentren una familia que les brinde amor incondicional. ¡Ayudanos a cumplir nuestra misión!</p>
-                </div>
             </div>
         )
     }
